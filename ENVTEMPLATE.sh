c2f3bf64330a833860d8a4c538c553f687d16e3a
Tavi Nathanson <tavi.nathanson@gmail.com>
2014-12-30 17:41:24 -0600

    Adding better config management

---
+++ ENVTEMPLATE.sh
@@ -0,0 +1,3 @@
+export PORT=5000
+export USE_RELOADER=False
+export DATABASE_URI='postgres:///patients:patients@localhost/patients'
---
+++ config.py
@@ -0,0 +1,10 @@
+def handle_false(value):
+    """Ensure that false in config isn't interpreted as True."""
+    if value and value.lower() == 'false':
+        value = False
+    return value
+
+USE_RELOADER = handle_false(os.environ.get('USE_RELOADER', False))
+PORT = int(os.environ.get('PORT', 5000))
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+
