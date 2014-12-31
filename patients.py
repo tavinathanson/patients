2ebe4befd424c27faaca62cdc048a24672b15aac
Tavi Nathanson <tavi.nathanson@gmail.com>
2014-12-31 00:53:37 -0600

    Use MethodView to remove REST boilerplate

--- patients.py
+++ patients.py
@@ -1,122 +1,57 @@
 from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.views import MethodView
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+
 # Must come after db initialization, as it uses that
 from models import Patient, Question, Answer
 
-@app.route('/patient', methods=['POST', 'GET'])
-def patient():
-    if request.method == 'POST':
-        return create_patient()
-    elif request.method == 'GET':
-        return get_patient()
+
+class PatientAPI(MethodView):
+    def get(self):
+        return None
+
+    
+patient_view = PatientAPI.as_view('patient_api')
+app.add_url_rule('/patient', view_func=patient_view)
 
 
-@app.route('/patient/<patient_id>', methods=['PUT', 'DELETE'])
-def patient_with_id(patient_id):
-    if request.method == 'PUT':
-        return update_patient(patient_id)
-    elif request.method == 'DELETE':
-        return delete_patient(patient_id)
+class QuestionAPI(MethodView):
+    def post(self):
+        # TODO(tavi) Error response if there's an issue.
+        data = dict(request.json)
+        conditional_on = None
+        if Question.conditional_on.name in data:
+            conditional_on = data[Question.conditional_on.name]
+        question = Question(text=data[Question.text.name],
+                            conditional_on=conditional_on)
+        db.session.add(question)
+        db.session.commit()
+        return success_response()
 
 
-def create_patient():
-    return None
+question_view = QuestionAPI.as_view('question_api')
+app.add_url_rule('/question', methods=['POST'], view_func=question_view)
 
 
-def get_patient():
-    return None
+class AnswerAPI(MethodView):
+    def post(self):
+        # TODO(tavi) Error response if there's an issue.
+        data = dict(request.json)
+        answer = Answer(text=data[Answer.text.name],
+                        question_id=data[Answer.question_id.name])
+        db.session.add(answer)
+        db.session.commit()
+        return success_response()
 
 
-def update_patient(patient_id):
-    return None
-
-
-def delete_patient(patient_id):
-    return None
-
-
-@app.route('/question', methods=['POST', 'GET'])
-def question():
-    if request.method == 'POST':
-        return create_question()
-    elif request.method == 'GET':
-        return get_question()
-
-
-@app.route('/question/<question_id>', methods=['PUT', 'DELETE'])
-def question_with_id(question_id):
-    if request.method == 'PUT':
-        return update_question(question_id)
-    elif request.method == 'DELETE':
-        return delete_question(question_id)
-
-
-def create_question():
-    # TODO(tavi) Error response if there's an issue.
-    data = dict(request.json)
-    conditional_on = None
-    if Question.conditional_on.name in data:
-        conditional_on = data[Question.conditional_on.name]
-    question = Question(text=data[Question.text.name],
-                        conditional_on=conditional_on)
-    db.session.add(question)
-    db.session.commit()
-    return success_response()
-
-
-def get_question():
-    return None
-
-
-def update_question(question_id):
-    return None
-
-
-def delete_question(question_id):
-    return None
-
-
-@app.route('/answer', methods=['POST', 'GET'])
-def answer():
-    if request.method == 'POST':
-        return create_answer()
-    elif request.method == 'GET':
-        return get_answer()
-
-
-@app.route('/answer/<answer_id>', methods=['PUT', 'DELETE'])
-def answer_with_id(answer_id):
-    if request.method == 'PUT':
-        return update_answer(answer_id)
-    elif request.method == 'DELETE':
-        return delete_answer(answer_id)
-
-
-def create_answer():
-    # TODO(tavi) Error response if there's an issue.
-    data = dict(request.json)
-    answer = Answer(text=data[Answer.text.name],
-                    question_id=data[Answer.question_id.name])
-    db.session.add(answer)
-    db.session.commit()
-    return success_response()
-
-
-def get_answer():
-    return None
-
-
-def update_answer(answer_id):
-    return None
-
-
-def delete_answer(answer_id):
-    return None
+answer_view = AnswerAPI.as_view('answer_api')
+app.add_url_rule('/answer', methods=['POST'], view_func=answer_view)
 
 
 def success_response():
